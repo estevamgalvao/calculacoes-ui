dedb503248f1034ae286d94a07b0b6122c9fb637
Estevam Albuquerque <estevamgalvao1@gmail.com>
2025-12-13 18:20:20 -0300

    feat: added custom theme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './features/home/pages/home/home.page';
+import { TestPage } from './features/home/pages/test/test.page';
 
 export const routes: Routes = [
   
-    {path: '', component: HomePage}
+    {path: '', component: HomePage},
+    {path: 'test', component: TestPage}
     
 ];
